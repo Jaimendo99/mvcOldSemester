2f9fe6de08697320800db9dddc0753b2c39a33f7
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-01-15 12:05:07 -0500

    Update static files resolution

--- web_mini_core/settings.py
+++ web_mini_core/settings.py
@@ -20,7 +20,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -51,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- web_mini_core/urls.py
+++ web_mini_core/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.views.generic import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path('app/', include('contracts.urls')),
     path('contracts/', RedirectView.as_view(url='/app/')), # Redirect on root
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
